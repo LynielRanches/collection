a2d21349f472687f9e071525d0e5bea1a70f2fe5
COLLEGELAB-09 <COLLEGELAB-09@BJSB.LAB>
2026-06-08 14:32:01 +0800

    Update Main.java

---
+++ src/collection.module/Main.java
@@ -0,0 +1,49 @@
+package edu.lynielranches;
+import java.util.*;
+
+public class Main { 
+    public static void main(String args[]) {
+
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Ana");
+        names.add("Mark");
+        names.add("Ana"); // duplicates allowed
+
+        Iterator<String> it = names.iterartor();
+        while (it.hasNext()) {
+            String n = it.next();
+            System.out.println(n);
+        }
+
+        for (String n: names){
+            System.out.println(n);
+        }
+           for (int i = 0; i < names.size(); i++) {
+            System.out.println(names.get(i).toString());
+        }
+
+        var animals = new ArrayList<Animal>();
+        var e = new Animal();
+        e.color = "Brown";
+        e.name = "Chris";
+        animals.add(e);
+        animals.add(new Animal("Black", "Ne"));
+
+        for(int i=0; i < animals.size(); i++) {
+            System.out.println(animals.get(i).name);
+        }
+        for(Animal animal : animals) {
+            
+        }
+    }
+
+    class Animal {
+        public String color;
+        public String name;
+        public Animal(){}
+        public Animal(String color, String name) {
+            this.color = color;
+            this.name = name;
+        }
+    }
+}
